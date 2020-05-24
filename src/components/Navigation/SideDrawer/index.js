8599d928406004914a1b4388a103c2cdf238f4bf
Martin Cserép <martincserep1@gmail.com>
2020-05-24 12:42:59 +0200

    code cleaning

--- src/components/Navigation/SideDrawer/index.js
+++ src/components/Navigation/SideDrawer/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../Logo/index.js';
-import NavigationItems from '../NavigationItems/index.js';
-import styles from './SideDrawer.module.scss';
-import Backdrop from '../../UI/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Logo from "../../Logo/index.js";
+import NavigationItems from "../NavigationItems/index.js";
+import styles from "./SideDrawer.module.scss";
+import Backdrop from "../../UI/Backdrop";
+import Aux from "../../../hoc/Aux";
 
-const sideDrawer = ( props ) => {
-    let attachedClasses = [styles.SideDrawer, styles.Close];
-    if (props.open) {
-        attachedClasses = [styles.SideDrawer, styles.Open];
-    }
-    return (
-        <Aux>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
-                <div className={styles.Logo}>
-                    <Logo />
-                </div>
-                <nav>
-                    <NavigationItems isAuthenticated={props.isAuth} />
-                </nav>
-            </div>
-        </Aux>
-    );
+const sideDrawer = (props) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+      </div>
+    </Aux>
+  );
 };
 
 export default sideDrawer;
